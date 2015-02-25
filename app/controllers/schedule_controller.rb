e03dffae9dafa824a71324b33f557b4cdb4aaec6
Doug <git@digitalfx.me.uk>
2015-02-25 19:58:03 +0000

    Display a schedule as unnamed if the name was left blank. Also fix missing days string for a new schedule.

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -93,8 +93,13 @@ class ScheduleController < UITableViewController
       cell ||= UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier: "ScheduleCell")
       schedule = @schedules[indexPath.row]
 
-      cell.textLabel.text = schedule.name
+      unless schedule.name.nil? || schedule.name.empty?
+        cell.textLabel.text = schedule.name
+      else
+        cell.textLabel.text = "Unnamed Schedule"
+      end
       cell.detailTextLabel.text = schedule.days_string
+      cell.detailTextLabel.setNeedsLayout   # needed to properly size the label if it was originally empty 
 
       cell
     end
